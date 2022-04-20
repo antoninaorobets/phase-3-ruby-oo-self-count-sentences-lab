8ea7dfc8809eeab596e18332a73c232d3dee0a7a
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-19 22:25:40 -0400

    commit tree 8732d4afeb61f2eb8a2b6374425f633bb1c33fa8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+      #  binding.pry
+    self.split(/[.+|!+|?+]/).filter{|str| str.length >0}.length
+
 
   end
 end
